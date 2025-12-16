a282a4d0aaf3a4f5dd31cf2bac77f09d281ca799
21-Ferdi-21 <155758596+21-Ferdi-21@users.noreply.github.com>
2025-12-16 16:51:16 +0100

    Create Categorie class with getter and setter

---
+++ Categorie.java
@@ -0,0 +1,20 @@
+
+public class Categorie
+{
+    private String nom;
+
+    public Categorie()
+    {
+        nom = null;
+    }
+
+    public String getNom()
+    {
+        return this.nom;
+    }
+    
+    public void setNom(String nom)
+    {
+        this.nom = nom;
+    }
+}
