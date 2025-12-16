c5b3b927f6d38031ed1ed6be1ded59ec77a6121f
21-Ferdi-21 <155758596+21-Ferdi-21@users.noreply.github.com>
2025-12-16 16:50:55 +0100

    Add Film class with attributes and methods

---
+++ Film.java
@@ -0,0 +1,51 @@
+import java.util.Date;
+
+public class Film
+{
+    private int duree;
+    private String dateSortie;
+    private Categorie categorie;
+
+    public Film()
+    {
+        // initialisation des variables d'instance
+        duree = 0;
+        dateSortie = "2001/01/01";
+    }
+
+    public int getDuree()
+    {
+        return this.duree;
+    }
+    
+    public void setDuree(int duree)
+    {
+        this.duree = duree;
+    }
+    
+    public String getDateSortie()
+    {
+        return this.dateSortie;
+    }
+    
+    public void setDateSortie(String dateSortie)
+    {
+        this.dateSortie = dateSortie;
+    }
+    
+    public void setCategorie(Categorie categorie)
+    {
+        this.categorie = categorie;
+    }
+    
+    public Categorie getCategorie()
+    {
+        return this.categorie;
+    }
+    
+    public String description()
+    {
+        return "Je suis un film de catégorie " + this.categorie.getNom() + " et de durée " + this.duree + " sorti en " + this.dateSortie;
+    }
+    
+}
